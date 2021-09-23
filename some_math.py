32c4beaa51b7f234549ca42619a5e34043b9edd8
kellar1995 <kellar_a@yahoo.com>
2021-09-22 23:20:52 -0500

    finished hw

---
+++ some_math.py
@@ -0,0 +1,7 @@
+def area_of(m1,m2):
+    area = m1 * m2
+    print("The area of your house is " + str(area) + " sq. ft.")
+
+def circum(r):
+    circle = 2 * 3.14 * r
+    print("The circumference of your circle is " + (str(circle)))
